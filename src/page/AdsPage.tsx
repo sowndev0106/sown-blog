d9a4f77ea0006bbab3338c881a1d9efaa055ba10
NGUYỄN THANH SƠN <thanhson.nguyen@lotte.vn>
2024-10-24 13:38:01 +0700

    add google ads code to page

--- src/page/AdsPage.tsx
+++ src/page/AdsPage.tsx
@@ -1,36 +1,34 @@
-import { Box, Grid, Typography, styled, Button, Chip } from '@mui/material';
-import Container from '@mui/material/Container';
-import Paper from '@mui/material/Paper';
-import CardMedia from '@mui/material/CardMedia';
-import ContactIcons from '../components/theme/contact-icon/ContactIcons';
-import { Divider } from '@mui/material';
-import AboutMeSection from '../sections/resume/AboutMeSection';
-import SkillSection from '../sections/resume/SkillSection';
-import ServiceSection from '../sections/resume/ServiceSection';
-import ExperienceSection from '../sections/resume/ExperienceSection';
-import EducationSection from '../sections/resume/EducationSection';
-import SideProjectSection from '../sections/resume/SideProjectSection';
-import DownloadIcon from '@mui/icons-material/Download';
-import { Link as RouterLink } from 'react-router-dom';
-const Root = styled(Container)(({ theme }) => ({
-    fontFamily: "-apple-system",
-    padding: theme.spacing(2),
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "calc(100vh - 100px)",
-    maxHeight: "1032px",
+import React, { useEffect } from 'react';
 
-}));
+const AdsPage: React.FC = () => {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6055563078016167";
+        script.async = true;
+        script.crossOrigin = "anonymous";
+        document.body.appendChild(script);
 
+        const adScript = document.createElement('script');
+        adScript.innerHTML = '(adsbygoogle = window.adsbygoogle || []).push({});';
+        document.body.appendChild(adScript);
 
-export default function AdsPage() {
+        return () => {
+            document.body.removeChild(script);
+            document.body.removeChild(adScript);
+        };
+    }, []);
 
     return (
-        <>
-            <Root>
-                <h1>ADS Page</h1>
-            </Root>
-        </>
-    )
-}
+        <div>
+            <h1>Ads Page</h1>
+            <ins className="adsbygoogle"
+                style={{ display: 'block' }}
+                data-ad-format="fluid"
+                data-ad-layout-key="-6t+ed+2i-1n-4w"
+                data-ad-client="ca-pub-6055563078016167"
+                data-ad-slot="8003613179"></ins>
+        </div>
+    );
+};
+
+export default AdsPage;
